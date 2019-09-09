fa3c6398e685f1c6d3804ce1eac65133990411bb
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2019-09-08 23:50:26 -0300

    alterações na navbar

--- i-do/src/components/navbar-home.js
+++ i-do/src/components/navbar-home.js
@@ -5,21 +5,22 @@ class Navbar extends React.Component {
     render() {
         return (
             <div>
-                <nav id="navbar" className="navbar navbar-light">
-                    <div className="row w-100">
-                        <div className="col-6">
-                            <a className="navbar-brand">Logo</a>
-                        </div>
-                        <div className="col-6">
-                            <form className="form-inline">
-                                <div className="col-3">
-                                    <button id="btnCadastroNavbar" className="btn w-100">Cadastre-se!</button>
+                <nav id="navbar" className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand">Logo</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <form className="row w-100 form-inline justify-content-end">
+                            <div className="row">
+                                <div className="col-xl-8 col-lg-8 col-md-8">
+                                    <button id="btnCadastroNavbar" className="form-control mr-sm-2">Cadastre-se!</button>
                                 </div>
-                                <div className="col-3">
-                                    <button id="btnLoginNavbar" class="btn my-2 my-sm-0 w-100" type="submit">Login</button>
+                                <div className="col-xl-4 col-lg-4 col-md-4">
+                                    <button id="btnLoginNavbar" className="btn my-2 my-sm-0" type="submit">Login</button>
                                 </div>
-                            </form>
-                        </div>
+                            </div>
+                        </form>
                     </div>
                 </nav>
             </div>
