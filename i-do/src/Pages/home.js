39dbea704cb5f51b2e9c6295c73f043aa63ae926
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2019-09-12 22:08:49 -0300

    continuação da construção da tela home e da navbar.

--- i-do/src/Pages/home.js
+++ i-do/src/Pages/home.js
@@ -1,57 +1,78 @@
 import React from 'react';
 import '../assets/css/home.css';
+import Navbar from '../components/navbar-home';
 
 class Home extends React.Component {
     render() {
         return (
-        <div id="body">
-            <div id="content" className="justify-content-center">
-                <div className="justify-content-center">
-                    <div className="col-xl-12" align="center">
-                        <h1 id="title">Vamos começar?</h1>
-                    </div>
-                    <div className="col-xl-12" align="center">
-                        <p id="text">Explore a variedade de serviços com a facilidade de divisão por categorias.</p>
-                    </div>
-                </div>
-                
-                <div className="row">
-                    <div className="col-xl-6">
-                        <div className="row">
-                            <div className="item-catalogo col bg-info" align="center">
-                                Aqui alguma categoria
+            <div id="body">
+                <fragment>
+                    <Navbar />
+                    <div id="content" className="justify-content-center mt-4">
+                        <div className="justify-content-center">
+                            <div className="col-xl-12" align="center">
+                                <h1 id="title">Vamos começar?</h1>
                             </div>
-                            <div className="item-catalogo col bg-info" align="center">
-                                Aqui alguma categoria
-                            </div>
-                            <div className="item-catalogo col bg-info" align="center">
-                                Aqui alguma categoria
-                            </div>
-                            <div className="item-catalogo col bg-info" align="center">
-                                Aqui alguma categoria
+                            <div className="col-xl-12 p-3" align="center">
+                                <p id="text">Explore a variedade de serviços com a facilidade de divisão por categorias.</p>
                             </div>
                         </div>
-                    </div>
-                    <div className="col-xl-6">
-                        <div className="row">
-                            <div className="item-catalogo col bg-info justify-content-center" align="center">
-                                Aqui alguma categoria
-                            </div>
-                            <div className="item-catalogo col bg-info" align="center">
-                                Aqui alguma categoria
-                            </div>
-                            <div className="item-catalogo col bg-info" align="center">
-                                Aqui alguma categoria
-                            </div>
-                            <div className="item-catalogo col bg-info" align="center">
-                                Aqui alguma categoria
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                        <div className="justify-content-center" align="center">
 
+                            <div className="row w-100">
+                                <div className="col-xl-6">
+                                    <div className="row">
+                                        <div className="p-1 col-3" align="center">
+                                            <div className="item-catalogo col bg-info">
+                                                Aqui alguma categoria
+                                        </div>
+                                        </div>
+                                        <div className="p-1 col-3" align="center">
+                                            <div className="item-catalogo col bg-info">
+                                                Aqui alguma categoria
+                                        </div>
+                                        </div>
+                                        <div className="p-1 col-3" align="center">
+                                            <div className="item-catalogo col bg-info">
+                                                Aqui alguma categoria
+                                        </div>
+                                        </div>
+                                        <div className="p-1 col-3" align="center">
+                                            <div className="item-catalogo col bg-info">
+                                                Aqui alguma categoria
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-xl-6">
+                                    <div className="row">
+                                        <div className="p-1 col-3" align="center">
+                                            <div className="item-catalogo col bg-info">
+                                                Aqui alguma categoria
+                                        </div>
+                                        </div>
+                                        <div className="p-1 col-3" align="center">
+                                            <div className="item-catalogo col bg-info">
+                                                Aqui alguma categoria
+                                        </div>
+                                        </div>
+                                        <div className="p-1 col-3" align="center">
+                                            <div className="item-catalogo col bg-info">
+                                                Aqui alguma categoria
+                                        </div>
+                                        </div>
+                                        <div className="p-1 col-3" align="center">
+                                            <div className="item-catalogo col bg-info">
+                                                Aqui alguma categoria
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </fragment>
             </div>
-        </div>
         )
     };
 }
