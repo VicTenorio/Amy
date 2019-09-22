e88fd8189eb37cc3145539a351101fcba426f3f9
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2019-09-21 21:48:52 -0300

    Atualização da tela home

--- i-do/src/Pages/home.js
+++ i-do/src/Pages/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../assets/css/home.css';
 import Navbar from '../components/navbar-home';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
     render() {
@@ -10,7 +11,7 @@ class Home extends React.Component {
                     <fragment>
                         <Navbar />
                         <div id="content" className="justify-content-center mt-4">
-                            <section id="start">
+                            <section name="start">
                                 <div className="justify-content-center mt-4">
                                     <div className="col-xl-12 p-3" align="center">
                                         <h1 id="title">Vamos começar?</h1>
@@ -23,50 +24,50 @@ class Home extends React.Component {
                                     <div className="row w-100">
                                         <div className="col-xl-6">
                                             <div className="row">
-                                                <div className="p-1 col-3" align="center">
+                                                <Link to="#" className="p-1 col-3" align="center">
                                                     <div className="item-catalogo col">
                                                         <i class="fa fa-automobile display-1"></i>
                                                     </div>
-                                                </div>
-                                                <div className="p-1 col-3" align="center">
+                                                </Link>
+                                                <Link to="#" className="p-1 col-3" align="center">
                                                     <div className="item-catalogo col">
                                                         <i class="fa fa-child display-1"></i>
                                                     </div>
-                                                </div>
-                                                <div className="p-1 col-3" align="center">
+                                                </Link>
+                                                <Link to="#" className="p-1 col-3" align="center">
                                                     <div className="item-catalogo col">
                                                         <i class="fa fa-bathtub display-1"></i>
                                                     </div>
-                                                </div>
-                                                <div className="p-1 col-3" align="center">
+                                                </Link>
+                                                <Link to="#" className="p-1 col-3" align="center">
                                                     <div className="item-catalogo col">
                                                         <i class="fa fa-code display-1"></i>
                                                     </div>
-                                                </div>
+                                                </Link>
                                             </div>
                                         </div>
                                         <div className="col-xl-6">
                                             <div className="row">
-                                                <div className="p-1 col-3" align="center">
+                                                <Link to="#" className="p-1 col-3" align="center">
                                                     <div className="item-catalogo col">
                                                         <i class="fa fa-cog display-1"></i>
                                                     </div>
-                                                </div>
-                                                <div className="p-1 col-3" align="center">
+                                                </Link>
+                                                <Link to="#" className="p-1 col-3" align="center">
                                                     <div className="item-catalogo col">
                                                         <i class="fa fa-cutlery display-1"></i>
                                                     </div>
-                                                </div>
-                                                <div className="p-1 col-3" align="center">
+                                                </Link>
+                                                <Link to="#" className="p-1 col-3" align="center">
                                                     <div className="item-catalogo col">
                                                         <i class="fa fa-futbol-o display-1"></i>
                                                     </div>
-                                                </div>
-                                                <div className="p-1 col-3" align="center">
+                                                </Link>
+                                                <Link to="#" className="p-1 col-3" align="center">
                                                     <div className="item-catalogo col">
                                                         <i class="fa fa-graduation-cap display-1"></i>
                                                     </div>
-                                                </div>
+                                                </Link>
                                             </div>
                                         </div>
                                     </div>
@@ -75,9 +76,12 @@ class Home extends React.Component {
                         </div>
                     </fragment>
                 </div>
-                <section id="solution">
-                    <div id="redonda" align="center">
+                <section name="solution">
+                    <div id="solution" align="center">
                         <div className="p-4">
+                            <div>
+                                <i id="light" className="fa fa-lightbulb-o display-1"></i>
+                            </div>
                             <div>
                                 <h1 className="display-4" id="meetSolution">Conheça nossas soluções</h1>
                             </div>
@@ -85,7 +89,7 @@ class Home extends React.Component {
                                 <h2 id="developedFor">Desenvolvidas para facilitar a conexão de prestadores e clientes</h2>
                             </div>
                         </div>
-                        <div className="row justify-content-center p-5">
+                        <div className="row w-100 justify-content-center p-5">
                             <div className="col">
                                 <div id="iNeed">
                                     <h1 id="titleIneed" align="left">I need</h1>
@@ -94,7 +98,7 @@ class Home extends React.Component {
                                     </p>
                                 </div>
                             </div>
-                            <div className="col">
+                            <div className="col ">
                                 <div id="iDo">
                                     <h1 id="titleIdo" align="left">I do</h1>
                                     <p className="description">Para quem busca profissionais  que prestem um serviço eficiente
@@ -105,20 +109,23 @@ class Home extends React.Component {
                         </div>
                     </div>
                 </section>
-                <section id="aboutUs">
-                    <footer className="us row">
+                <section name="aboutUs">
+                    <footer className="us row w-100">
                         <div className="col">
-                            <p className="description">Somos Amy! idealizada a contornar
+                            <p className="description mt-3 mb-5 p-5">Somos Amy! idealizada a contornar
                                 as dificuldades de comunicação, organização e
                                 tempo na hora de prestar serviços ou contratar um prestador
                                 de modo ágil.
                             </p>
                         </div>
                         <div className="col">
-                            <h1>Redes Socias</h1>
-                            <i>instagram</i>
-                            <i>linkedin</i>
-                            <i>twitter</i>
+                            <div className="social mt-3 mb-5 p-5">
+                                <h1>Redes Socias</h1>
+                                <i className="icons-social fa fa-facebook-square p-1"></i>
+                                <i className="icons-social fa fa-instagram p-1"></i>
+                                <i className="icons-social fa fa-linkedin-square p-1"></i>
+                                <i className="icons-social fa fa-tumblr-square p-1"></i>
+                            </div>
                         </div>
                     </footer>
                 </section>
