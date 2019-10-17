7146550e18c92809f85a8df5c3874cb0e88353ce
Matheus Vicente <mvicente@outlook.com.br>
2019-10-16 22:15:26 -0300

    Callback adicionado

--- Me&MrJones/src/controllers/usuarioController.js
+++ Me&MrJones/src/controllers/usuarioController.js
@@ -1,16 +1,17 @@
-
 var Maps = require('../models/mapsModel');
 var Conn = require('../models/connection');
 var Usuario = require('../models/usuarioModel');
 
  exports.selectAll = (req,res,next)=>{    
-    new Usuario().usuario.selectAll(res);
+    new Usuario().usuario.selectAll((result)=>{
+        res.send(result);
+    });
  }
 
-
- 
  exports.login = (req, res, next) => {    
     const email = req.body.email;
     const senha = req.body.senha;
-    const user = new Usuario().login(email,senha,res);
+    const user = new Usuario().login(email,senha,(result)=>{
+        res.send(result);
+    });
 }
--- Me&MrJones/src/models/usuarioModel.js
+++ Me&MrJones/src/models/usuarioModel.js
@@ -35,12 +35,12 @@ module.exports = class Usuario {
         const userList = [];
         sql.connect(config).then(() => {
             return sql.query`select * from usuario`
-        }).then(resultado => {            
-            resultado.recordset.forEach(x =>{                                    
+        }).then(resultado => {
+            resultado.recordset.forEach(x => {
                 const user = new Usuario();
                 user.idUsuario = x["id_usuario"].toString();
                 user.RG = x["rg"];
-                user.CpfCnpj =  x["cpf_cnpj"];
+                user.CpfCnpj = x["cpf_cnpj"];
                 user.dataNascimento = x["data_nascimento"];
                 user.email = x["email"];
                 user.estadoCivil = x["estado_civil"];
@@ -51,23 +51,21 @@ module.exports = class Usuario {
                 user.telefone = x["telefone"];
                 userList.push(user);
             });
-            res.send(user);
         }).catch(err => {
             res.json(err);
-        })
-
+        });
         sql.close();
+        res(user);
     }
 
     login(email, senha, res) {
+        const user = new Usuario();
         sql.connect(config).then(async () => {
-            var resultado = await sql.query`select * from usuario where email = ${email} and senha = ${senha}`
-            
-            const user = new Usuario();
-            resultado.recordset.forEach(x =>{                                    
+            var resultado = await sql.query`select * from usuario where email = ${email} and senha = ${senha}`            
+            resultado.recordset.forEach(x => {
                 user.idUsuario = x["id_usuario"].toString();
                 user.RG = x["rg"];
-                user.CpfCnpj =  x["cpf_cnpj"];
+                user.CpfCnpj = x["cpf_cnpj"];
                 user.dataNascimento = x["data_nascimento"];
                 user.email = x["email"];
                 user.estadoCivil = x["estado_civil"];
@@ -76,13 +74,12 @@ module.exports = class Usuario {
                 user.senha = x["senha"];
                 user.sexo = x["sexo"];
                 user.telefone = x["telefone"];
+                res(user);
             });
             sql.close();
-            res.send(user);
+        }).catch(err => {
+            res.json(err);
+            console.log("eroooo");
         })
-            .catch(err => {
-                res.json(err);
-                console.log("eroooo");
-            })
     }
 }
