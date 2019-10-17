121dd2fab9f1cc22365a0cc13476ce16ae1bd419
Matheus Vicente <mvicente@outlook.com.br>
2019-10-16 21:52:38 -0300

    Alterações na classe de retorno

--- Me&MrJones/src/models/usuarioModel.js
+++ Me&MrJones/src/models/usuarioModel.js
@@ -3,89 +3,86 @@ var config = require("../models/connection");
 
 module.exports = class Usuario {
 
-    selectAll(res){
-        
+    constructor(idUsuario,
+        nome,
+        email,
+        CpfCnpj,
+        RG,
+        telefone,
+        senha,
+        prestador,
+        sexo,
+        estadoCivil,
+        dataNascimento,
+        idEndereco, ) {
+        this.idUsuario = idUsuario;
+        this.nome = nome;
+        this.email = email;
+        this.CpfCnpj = CpfCnpj;
+        this.RG = RG;
+        this.telefone = telefone;
+        this.senha = senha;
+        this.prestador = prestador;
+        this.sexo = sexo;
+        this.estadoCivil = estadoCivil;
+        this.dataNascimento = dataNascimento;
+        this.idEndereco = idEndereco;
+
+    }
+
+
+    selectAll(res) {
+        const userList = [];
         sql.connect(config).then(() => {
             return sql.query`select * from usuario`
-        }).then(result => {
-            res.json(result);
+        }).then(resultado => {            
+            resultado.recordset.forEach(x =>{                                    
+                const user = new Usuario();
+                user.idUsuario = x["id_usuario"].toString();
+                user.RG = x["rg"];
+                user.CpfCnpj =  x["cpf_cnpj"];
+                user.dataNascimento = x["data_nascimento"];
+                user.email = x["email"];
+                user.estadoCivil = x["estado_civil"];
+                user.nome = x["nome"];
+                user.prestador = x["prestador"];
+                user.senha = x["senha"];
+                user.sexo = x["sexo"];
+                user.telefone = x["telefone"];
+                userList.push(user);
+            });
+            res.send(user);
         }).catch(err => {
             res.json(err);
         })
 
         sql.close();
-        //return connect(query,call => {console.log(call.recordset)});
     }
 
-    login(email,senha,res){
+    login(email, senha, res) {
         sql.connect(config).then(async () => {
             var resultado = await sql.query`select * from usuario where email = ${email} and senha = ${senha}`
-            console.log(">>>"+email);
-            return res.json(resultado);            
+            
+            const user = new Usuario();
+            resultado.recordset.forEach(x =>{                                    
+                user.idUsuario = x["id_usuario"].toString();
+                user.RG = x["rg"];
+                user.CpfCnpj =  x["cpf_cnpj"];
+                user.dataNascimento = x["data_nascimento"];
+                user.email = x["email"];
+                user.estadoCivil = x["estado_civil"];
+                user.nome = x["nome"];
+                user.prestador = x["prestador"];
+                user.senha = x["senha"];
+                user.sexo = x["sexo"];
+                user.telefone = x["telefone"];
+            });
+            sql.close();
+            res.send(user);
         })
-        .catch(err => {
-            res.json(err);
-            console.log("eroooo");
-        })
-
-        //sql.close();
-        //return connect(query,call => {console.log(call.recordset)});
+            .catch(err => {
+                res.json(err);
+                console.log("eroooo");
+            })
     }
-/*
-    selectAll(res){
-        var sqlQry = "select * from usuarios;"
-        conn.connection().query(sqlQry, function(err, results, fields){
-            if(err) res.json(err);
-            else return res.json(results);
-            connection.end();                    
-        })
-    }
-
-    selectById(id,res){
-        var sqlQry = "select * from usuarios where id = " + id;
-        conn.connection().query(sqlQry, function(err, results, fields){
-            if(err) res.json(err);
-            else return res.json(results);
-            connection.end();
-        })
-    }
-
-    insertUser(email,senha,res){
-        var sqlQry = "insert into usuarios (email,senha) values ('"+email+"','"+senha+"');";
-        conn.connection().query(sqlQry, function(err, results, fields){
-            if(err) res.json(err);
-            else return res.json(results);
-            connection.end();
-        })
-    }
-
-    updateUser(id,email,senha,res){
-        var sqlQry = "update usuarios set email ='"+email+"', senha = '"+senha+"'"+
-        " where id = "+id;
-
-        conn.connection().query(sqlQry,function(err, results, fields){
-            if(err) res.json(err);
-            else return res.json(results);
-            connection.end();
-        })
-    }
-
-    deleteUser(id,res){
-        var sqlQry = "delete from usuarios where id = " + id;
-        conn.connection().query(sqlQry, function(err, results, fields){
-            if(err) res.json(err);
-            else return res.json(results);
-            connection.end();
-        })
-    }
-
-    login(email,senha,res){
-        var sqlQry = "select * from usuarios where email = '"+email+"' and senha = '"+senha+"';"
-
-        conn.connection().query(sqlQry, function(err, results, fields){
-            if(err) res.json(err);
-            else return res.json(results);
-            connection.end();
-        })
-    }*/
 }
