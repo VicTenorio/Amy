023b79bc8ae170334f0646e99e0206cdd49045b9
VicTenorio <vitoriabasto17@gmail.com>
2019-11-04 10:29:07 -0300

    cadastro

--- i-do/src/Pages/userRegister.js
+++ i-do/src/Pages/userRegister.js
@@ -14,11 +14,54 @@ class userRegister extends React.Component {
                     </div>
                     <div className="row">
                         <div class="row w-100">
-                            <p>Nome</p>
+                            <p class="txt-input">Nome</p>
                         </div>
                         <div class="row w-100">
                             <input class="input-name" />
                         </div>
+                    </div>
+                    <div className="row">
+                        <div class="col-4 w-100">
+                            <p class="txt-input">RG</p>
+                            <input class="input-inp" />
+                        </div>
+                        <div class="col-4 w-100">
+                            <p class="txt-input" style={{ marginLeft: '18%'}}>CPF</p>
+                            <input class="input-inp" style={{ marginLeft: '18%'}} />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div class="col-4 w-100">
+                            <p class="txt-input">Data de Nascimento</p>
+                            <input class="input-inp" />
+                        </div>
+                        <div class="col-4 w-100">
+                            <p class="txt-input" style={{ marginLeft: '18%'}}>Estado Civil</p>
+                            <div class="dropdown">
+                                <button class="btn input-btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Selecione
+                                </button>
+                            <div class="dropdown-menu w-100" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">Solteiro(a)</a>
+                                <a class="dropdown-item" href="#">Casado(a)</a>
+                                <a class="dropdown-item" href="#">Divorciado(a)</a>
+                                <a class="dropdown-item" href="#">Viuva(a)</a>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                    <div class="row w-100">
+                        <p class="txt-input">Sexo</p>
+                    </div>
+                    <div class="row w-100">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" checked/>
+                        <label class="form-check-label">
+                            Feminino
+                        </label>
+                    </div>
+                    </div>
                     </div>
                     <div class="row title-register">
                         <p>Dados de Acesso e Contato</p>
