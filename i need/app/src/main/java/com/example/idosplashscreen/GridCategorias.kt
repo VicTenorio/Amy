d86b277d105f417834181aff6598e2ed17063cd3
wesley49 <wesley.wesley.bezerra49@hotmail.com>
2019-11-13 22:13:18 -0300

    ajustes Grid

--- i need/app/src/main/java/com/example/idosplashscreen/GridCategorias.kt
+++ i need/app/src/main/java/com/example/idosplashscreen/GridCategorias.kt
@@ -58,14 +58,14 @@ class GridCategorias : AppCompatActivity() {
 
         itemList = ArrayList()
 
-        itemList.add(MyModel(R.drawable.me_time, "Me"))
-        itemList.add(MyModel(R.drawable.family_time, "family"))
-        itemList.add(MyModel(R.drawable.lovely_time, "lovely"))
-        itemList.add(MyModel(R.drawable.team_time, "team"))
-        itemList.add(MyModel(R.drawable.friends, "friends"))
-        itemList.add(MyModel(R.drawable.calendar, "calendar"))
-        itemList.add(MyModel(R.drawable.me_time, "MW"))
-        itemList.add(MyModel(R.drawable.me_time, "MW"))
+        itemList.add(MyModel(R.drawable.car, getString(R.string.grid_auto)))
+        itemList.add(MyModel(R.drawable.website, getString(R.string.grid_informaticaTecnologia)))
+        itemList.add(MyModel(R.drawable.house, getString(R.string.grid_Domesticos)))
+        itemList.add(MyModel(R.drawable.brickwall, getString(R.string.grid_construção)))
+        itemList.add(MyModel(R.drawable.mortarboard, getString(R.string.grid_educacao)))
+        itemList.add(MyModel(R.drawable.box, getString(R.string.grid_entregasEncomendas)))
+        itemList.add(MyModel(R.drawable.salon, getString(R.string.grid_exteticaModa)))
+        itemList.add(MyModel(R.drawable.calendario, getString(R.string.grid_eventosMusicasDecoracoes)))
     }
 
 }
