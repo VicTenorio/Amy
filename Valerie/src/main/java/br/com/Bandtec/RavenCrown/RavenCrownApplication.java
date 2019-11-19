3e5f1a66529ea363376d0e6e758a2a7e97f6cd15
Matheus Vicente <mvicente@outlook.com.br>
2019-11-18 21:44:36 -0300

    Alterações no acesso ao banco e adaptação na main para o apache tomcat.

--- Valerie/src/main/java/br/com/Bandtec/RavenCrown/RavenCrownApplication.java
+++ Valerie/src/main/java/br/com/Bandtec/RavenCrown/RavenCrownApplication.java
@@ -2,9 +2,10 @@ package br.com.Bandtec.RavenCrown;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RavenCrownApplication {
+public class RavenCrownApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RavenCrownApplication.class, args);
