3333b3e1d21bbc0764e67bb71e4829964454c5c1
wesley49 <wesley.wesley.bezerra49@hotmail.com>
2019-11-27 01:19:25 -0300

    home usuario mobile

---
+++ i need/app/src/main/java/com/example/idosplashscreen/Home2Activity.kt
@@ -0,0 +1,32 @@
+package com.example.idosplashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.example.idosplashscreen.Model.UsuarioLogado
+
+class Home2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home2)
+
+        val id = intent.getIntExtra("idUsuario", 0)
+        val nome = intent.getStringExtra("nome")
+
+        var txt_nome = findViewById(R.id.nome_usuario) as TextView
+
+        txt_nome.text = " nome:"+nome.toString() +" id:"+ id.toString()
+    }
+
+    fun logout(v:View){
+        var login = Intent(this, LoginActivity::class.java)
+
+        UsuarioLogado.idUsuario = 0
+        UsuarioLogado.nomeUsuario = ""
+
+        startActivity(login)
+    }
+}
--- i need/app/src/main/java/com/example/idosplashscreen/HomeActivity.kt
+++ i need/app/src/main/java/com/example/idosplashscreen/HomeActivity.kt
@@ -1,32 +1,127 @@
 package com.example.idosplashscreen
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import android.view.Menu
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
-import com.example.idosplashscreen.Model.UsuarioLogado
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.idosplashscreen.Adapter.MyAdapter
+import com.example.idosplashscreen.Common.Common
+import com.example.idosplashscreen.Common.SpacesItemDescoration
+import com.example.idosplashscreen.Model.MyModel
+import kotlinx.android.synthetic.main.activity_grid_categorias.*
+import kotlinx.android.synthetic.main.activity_grid_categorias.recycler_view
+import kotlinx.android.synthetic.main.content_home.*
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.nav_header_home.*
+import kotlinx.android.synthetic.main.nav_header_home.view.*
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var adapter: MyAdapter
+    lateinit var itemList:MutableList<MyModel>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home
+            ), drawerLayout
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
         val id = intent.getIntExtra("idUsuario", 0)
         val nome = intent.getStringExtra("nome")
+        
 
-        var txt_nome = findViewById(R.id.nome_usuario) as TextView
+        val navigationView = findViewById<View>(R.id.nav_view) as NavigationView
+        val headerView = navigationView.getHeaderView(0)
+        val navNameText = headerView.findViewById<View>(R.id.name_menu) as TextView
+        val navEmailText = headerView.findViewById<View>(R.id.email_menu) as TextView
+        navNameText.text = nome.toString()
+        navEmailText.text = id.toString()
 
-        txt_nome.text = " nome:"+nome.toString() +" id:"+ id.toString()
+        InitData()
+        setData()
     }
 
-    fun logout(v:View){
-        var login = Intent(this, LoginActivity::class.java)
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.home, menu)
+        return true
+    }
 
-        UsuarioLogado.idUsuario = 0
-        UsuarioLogado.nomeUsuario = ""
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 
-        startActivity(login)
+    private fun setData() {
+        //============================
+        // Construção do Grid Layout
+        //============================
+        adapter = MyAdapter(this,itemList) // instancia do adapter
+
+        val layoutManager = GridLayoutManager(this, Common.NUM_OF_COLUMN)// Define o numero de columas do Grid
+        layoutManager.orientation = RecyclerView.VERTICAL // define o orientarion do Grid
+        layoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup(){
+            override fun getSpanSize(i: Int): Int {
+                return if (adapter != null){
+                    when(adapter!!.getItemViewType(i))
+                    {
+                        1 -> 1
+                        0 -> Common.NUM_OF_COLUMN
+                        else -> - 1
+                    }
+                }else{
+                    -1
+                }
+            }
+        }
+
+        recycler_view.layoutManager = layoutManager
+        recycler_view.addItemDecoration(SpacesItemDescoration(6))
+        recycler_view.adapter = adapter
+
+    }
+
+    private fun InitData(){
+        itemList = ArrayList()
+
+        itemList.add(MyModel(1,R.drawable.car, getString(R.string.grid_auto)))
+        itemList.add(MyModel(2,R.drawable.website, getString(R.string.grid_informaticaTecnologia)))
+        itemList.add(MyModel(3,R.drawable.house, getString(R.string.grid_Domesticos)))
+        itemList.add(MyModel(4,R.drawable.brickwall, getString(R.string.grid_construção)))
+        itemList.add(MyModel(5,R.drawable.mortarboard, getString(R.string.grid_educacao)))
+        itemList.add(MyModel(6,R.drawable.box, getString(R.string.grid_entregasEncomendas)))
+        itemList.add(MyModel(7,R.drawable.salon, getString(R.string.grid_exteticaModa)))
+        itemList.add(MyModel(8,R.drawable.calendario, getString(R.string.grid_eventosMusicasDecoracoes)))
     }
 }
