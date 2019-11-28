8fbe9b03a01cf88f4fb326faccc92f47692a603d
bcsantos1 <brunopapamike18@gmail.com>
2019-11-27 22:05:35 -0300

    nova tela de detelhes

---
+++ i need/app/src/main/java/com/example/idosplashscreen/DetalhesServico.kt
@@ -0,0 +1,20 @@
+package com.example.idosplashscreen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetalhesServico : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes_servico)
+
+
+
+
+
+
+
+    }
+}
