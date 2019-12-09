99a02f24ead1a0795c617ebb27d5b7c202502c5d
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2019-12-09 13:51:50 -0300

    Requisição da tela de meus agendamentos.

--- i-do/src/Pages/scheduledServices.js
+++ i-do/src/Pages/scheduledServices.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import Axios from 'axios'
 import '../assets/css/scheduledServices.css';
 import Navbar from '../components/navbarLogged';
-import CardScheduleServices from '../components/cardScheduleServices';
+import contract from '../Requests/ConnectionString';
+import ModalServiceDetails from '../Pages/modalServiceDetails';
 
 class ScheduledServices extends React.Component {
-    
 
+    state = {
+        contrato: []
+    };
+
+    componentWillMount() {
+
+        Axios.get(contract() + '/Prestador/Contratos?id=53' /*Prestador/Servicos?id=54*/)
+            .then(Response => {
+                console.log(Response.data)
+                this.setState({ contrato: Response.data });
+            })
+            .catch(Error => {
+                console.log("erro", Error)
+            })
+    }
 
     render() {
         return (
@@ -30,7 +46,89 @@ class ScheduledServices extends React.Component {
                     </div>
                 </div>
                 {/* <label id="noRegister" className="w-100" align="center">Usuário, você ainda não possui serviços agendados.</label> */}
-                <CardScheduleServices/>
+                {this.state.contrato.map(item => {
+                    return (
+                        <div className="row ml-1 mr-1 mt-4 " align="center">
+                            <div className="cardContainer col-xl-3 col-lg-3 col-md-3 col-sm-10" align="center">
+                                <label id="DayOfService">
+                                    {item.dtAgendamento}
+                                </label>
+                                <hr />
+                                <div className="p-3">
+                                    <label className="row">
+                                        Cliente: {item.consumidor.nome}
+                                    </label>
+                                    <label className="row">
+                                        Telefone: {item.consumidor.telefone}
+                                    </label>
+                                    <label className="row">
+                                        E-mail: {item.consumidor.email}
+                                    </label>
+                                </div>
+                                <div>
+                                    <input className="w-50" id="btnDetailsService" type="button" value="Analisar proposta" data-toggle="modal" data-target="#staticBackdrop" />
+                                </div>
+                                <div class="modal fade" id="staticBackdrop" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                        <div id="modalServiceDetails" class="modal-content">
+                                            <div class="modal-header">
+                                                <h4 class="modal-title" id="exampleModalCenterTitle">Detalhes do serviço agendado</h4>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div className="p-2">
+                                                    <div className="row">
+                                                        <label className="dataClient">Cliente: </label>
+                                                        <label>
+                                                            <em>
+                                                                {item.consumidor.nome}
+                                                            </em>
+                                                        </label>
+                                                    </div>
+                                                    <div className="row">
+                                                        <label className="dataClient">Telefone: </label>
+                                                        <label>
+                                                            <em>
+                                                                {item.consumidor.telefone}
+                                                            </em>
+                                                        </label>
+                                                    </div>
+                                                    <div className="row">
+                                                        <label className="dataClient">E-mail: </label>
+                                                        <label>
+                                                            <em>
+                                                                {item.consumidor.email}
+                                                            </em>
+                                                        </label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div id="modalFooter">
+                                                <div class="modal-footer justify-content-center" id="AproveButtons">
+                                                    <input type="button" className="w-25" id="btnReprovar" data-dismiss="modal" value="Reprovar" />
+                                                    <input type="button" className="w-25" id="btnAprovar" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample" value="Aprovar" />
+                                                </div>
+                                            </div>
+                                            <div class="collapse" id="collapseExample">
+                                                <div class="card card-body">
+                                                    <label>
+                                                        Você tem certeza que quer confirmar este agendamento?
+                                                </label>
+                                                    <div className="justify-content-center row w-100">
+                                                        <input type="button" className="col-xl-4" id="btnRate" data-toggle="collapse" data-target="#collapseExample" value="Avaliar melhor" />
+                                                        <input type="button" className="col-xl-4" id="btnConfirm" data-toggle="modal" data-target=".bd-example-modal-sm" value="Confirmar!" />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
